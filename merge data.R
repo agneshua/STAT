9f5fbe8e66873ee835b0e8a203ace8f43fe264db
agneshua <gpr7xd@virginia.edu>
2024-10-26 23:52:57 -0400

    Update merge data.R

--- merge data.R
+++ merge data.R
@@ -4,3 +4,7 @@ all_data <- lapply(file_list, read.csv)
 merged_data <- do.call(rbind, all_data)
 
 write.csv(merged_data, "merged_dataset.csv", row.names = FALSE)
+
+git add merged_dataset.csv
+git commit -m "Add merged dataset"
+git push origin main
