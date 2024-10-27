9551873bf13985aac89e7d0968239b43348b69bf
agneshua <gpr7xd@virginia.edu>
2024-10-26 23:49:47 -0400

    Update merge data.R

--- merge data.R
+++ merge data.R
@@ -1,1 +1,6 @@
+file_list <- list.files(path = "STAT", pattern = "*.csv", full.names = TRUE)
 
+all_data <- lapply(file_list, read.csv)
+merged_data <- do.call(rbind, all_data)
+
+write.csv(merged_data, "merged_dataset.csv", row.names = FALSE)
